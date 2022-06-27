4f0d22178bece17bf1b5d8e34004f3959afb111e
ShineTiger <555ysng@gmail.com>
2022-06-27 23:19:57 +0900

    :sparkles: FEAT : 초 카운트 시작기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [second,setSecond] = useState(0);
+
+  const countingSecond = () => {
+    setSecond(second+1);
+    console.log(second)
+  }
+
+  //카운트 시작버튼
+  const startBtn = () => {
+    setInterval(countingSecond,1000)
+  }
+
   return (
     <div className="App">
+      <button onClick={startBtn}>시작</button>
+      <p>{second}</p>
     </div>
   );
 }
