3bb4380b41ff1c4fbc63d04cd603031529a20a29
ShineTiger <555ysng@gmail.com>
2022-07-03 21:16:04 +0900

    :zap: 준비타이머, 초 카운터, 스쿼트횟수 카운터 기능 개발

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,59 @@ import "./App.css";
 import useInterval from "./hooks/useInterval";
 
 function App() {
+  const [timer, setTimer] = useState<number>(0);
+  const [isReady, setReady] = useState<boolean>(false);
   const [second, setSecond] = useState<number>(0);
   const [isPlaying, setPlaying] = useState<boolean>(false);
-  const delay: number = 1000;
+  const [squatTime, setSquatTime] = useState<number>(0);
 
+  //준비타이머
+  useInterval(
+    () => {
+      setTimer(timer + 1);
+    },
+    isReady ? 1000 : null
+  );
+
+  //준비타이머 종료
+  useEffect(() => {
+    if (timer === 3) {
+      setReady(false);
+      setPlaying(!isPlaying);
+    }
+  }, [timer]);
+
+  //초 카운터
   useInterval(
     () => {
       setSecond(second + 1);
     },
-    isPlaying ? delay : null
+    isPlaying ? 1000 : null
   );
 
+  //스쿼트횟수 카운터
   useEffect(() => {
-    if (second === 2) {
-      setPlaying(!isPlaying);
+    for (let i = 1; i < 4; i++) {
+      if (second === 10 * i) {
+        setSquatTime((squat) => squat + 1);
+      }
     }
   }, [second]);
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setPlaying(!isPlaying);
-        }}
-      >
-        시작
-      </button>
-      <p>{second}</p>
+      <div>
+        <button
+          onClick={() => {
+            setReady(!isReady);
+          }}
+        >
+          시작
+        </button>
+        <p>{timer}</p>
+        <p>{second}</p>
+        <p>{squatTime}</p>
+      </div>
     </div>
   );
 }
