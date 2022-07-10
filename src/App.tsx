973fa1f246a838be2b3927e27e7f55cfe598737f
ShineTiger <shinetiger.dev@gmail.com>
2022-07-10 19:03:59 +0900

    :recycle: Main 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,14 @@
 import { useState, useEffect } from "react";
 import useInterval from "./hooks/useInterval";
 import Ready from "./pages/Ready";
+import Main from "./pages/Main";
 
 function App() {
-  const [second, setSecond] = useState<number>(0);
-  const [isPlaying, setPlaying] = useState<boolean>(false);
-  const [squatTime, setSquatTime] = useState<number>(0);
-  const [squatSet, setSquatSet] = useState<number>(0);
-
-  //초 카운터
-  const SecondCounter = () => {
-    useInterval(
-      () => {
-        setSecond(second + 1);
-      },
-      isPlaying ? 1000 : null
-    );
-    return (
-      <>
-        <p>{second}</p>
-      </>
-    );
-  };
-
-  //10초 리셋
-  useEffect(() => {
-    if (second === 10) {
-      setSecond(0);
-      setSquatTime((squat) => squat + 1); //횟수 카운터
-    }
-  }, [second]);
-
-  //세트 카운터
-  useEffect(() => {
-    if (squatTime === 10) {
-      setSquatSet((set) => set + 1);
-    }
-  }, [squatTime]);
-
   return (
     <div className="App">
       <div>
         <Ready />
-        <SecondCounter />
-        <p>{squatTime}</p>
-        <p>{squatSet}</p>
+        <Main />
       </div>
     </div>
   );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import useInterval from "../hooks/useInterval";
+
+const Main = () => {
+  const [second, setSecond] = useState<number>(0);
+  const [isPlaying, setPlaying] = useState<boolean>(false);
+  const [squatTime, setSquatTime] = useState<number>(0);
+  const [squatSet, setSquatSet] = useState<number>(0);
+
+  //초 카운터
+  const SecondCounter = () => {
+    useInterval(
+      () => {
+        setSecond(second + 1);
+      },
+      isPlaying ? 1000 : null
+    );
+    return (
+      <>
+        <p>{second}</p>
+      </>
+    );
+  };
+
+  //10초 리셋
+  useEffect(() => {
+    if (second === 10) {
+      setSecond(0);
+      setSquatTime((squat) => squat + 1); //횟수 카운터
+    }
+  }, [second]);
+
+  //세트 카운터
+  useEffect(() => {
+    if (squatTime === 10) {
+      setSquatSet((set) => set + 1);
+    }
+  }, [squatTime]);
+
+  return (
+    <div>
+      <SecondCounter />
+      <p>{squatTime}</p>
+      <p>{squatSet}</p>
+    </div>
+  );
+};
+
+export default Main;
