ed60d58ecf7df13aa87424c3c7af51568fed15a7
ShineTiger <shinetiger.dev@gmail.com>
2022-08-05 01:27:57 +0900

    :recycle: useNavigate 대신 Link to로 변경
    useNavigate 훅을 페이지마다 반복해서 써서 줄이려고 함.

--- src/pages/Rest.tsx
+++ src/pages/Rest.tsx
@@ -1,40 +1,35 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import SecondTimer from "../components/SecondTimer";
 
 const Rest = () => {
   const [timer, setTimer] = useState(60);
-  const [isReady, setReady] = useState(false);
   const [isPlaying, setPlaying] = useState(false);
   const play = true;
 
   const navigate = useNavigate();
 
-  //휴식끝내기
-  const overRest = () => {
-    navigate("/Ready");
-  };
-
   //휴식타이머 종료
   useEffect(() => {
     if (timer === 0) {
-      setReady(false);
       setPlaying(!isPlaying);
-      navigate("/Main");
+      navigate("/Ready");
     }
   }, [timer]);
 
   return (
     <>
-      <button onClick={overRest} className="circle-btn inline-block">
-        <SecondTimer
-          second={timer}
-          setSecond={setTimer}
-          isPlay={play}
-          className="font-mono text-6xl"
-        />
-        <p className="text-2xl">휴식끝내기</p>
-      </button>
+      <Link to="/Ready">
+        <button className="circle-btn inline-block">
+          <SecondTimer
+            second={timer}
+            setSecond={setTimer}
+            isPlay={play}
+            className="font-mono text-6xl"
+          />
+          <p className="text-2xl">휴식끝내기</p>
+        </button>
+      </Link>
     </>
   );
 };
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,17 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Start = () => {
-  const navigate = useNavigate();
-
-  //시작버튼
-  const togglePlay = () => {
-    navigate("/Ready");
-  };
-
   return (
-    <button onClick={togglePlay} className="circle-btn">
-      시작
-    </button>
+    <Link to="/Ready">
+      <button className="circle-btn">시작</button>
+    </Link>
   );
 };
 
